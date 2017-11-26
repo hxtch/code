a46bca205a8a59b4b015054deaf8de0a673272c6
hxtch <hxtmic@outlook.com>
2017-11-26 19:49:18 +0800

    Update webdir_scan.py

--- pyfiles/webdir_scan.py
+++ pyfiles/webdir_scan.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python
+# -*- coding: utf-8 -*-
 
 import re
 import sys
@@ -7,10 +8,12 @@ import threading
 import argparse
 from multiprocessing.dummy import Pool
 
+
 TOTAL = 0
-COUNT = 0
-WIDTH = 50
-	
+COUNT = 1
+WIDTH = 60
+OUTPUT_LOCK = threading.Lock()
+
 headers = {
 	'Accept': '*/*',
 	'Referer': 'https://www.baidu.com',
@@ -18,60 +21,66 @@ headers = {
 	'Cache-Control': 'no-cache',
 }
 
+f = open('effienturl2.txt','wb')
+
 class Finished(Exception):
-	pass
+	print "finished"
 
-
-def send_request(url):
-	global COUNT
-	r = requests.get(url,headers = headers, allow_redirects = False,timeout = 20)
-	notfoundpagetext = requests.get(url + '/notexitstktdhhhhhh',allow_redirects = False).text
-	if r.status_code == 200 and r.text != notfoundpagetext:
-		COUNT+=1
-
-
-OUTPUT_LOCK = threading.Lock()
 def progress_bar():
 	OUTPUT_LOCK.acquire()
 	sys.stdout.write('\r\033[K')
-	finished = COUNT*100/TOTAL
+	finished = COUNT * 100 / TOTAL
 	finished_width = COUNT * WIDTH / TOTAL
 	unfinished_width = WIDTH - finished_width
 	sys.stdout.write(str(finished)+"%" + '[' \
-			+ ' \033[32m=\033[0m' * (finished_width) \
-			+ " \033[32m=\033[0m" \
-			+ '\033[32m=\033[0m' * unfinished_width + ']')
+			+ '\033[32m=\033[0m' * (finished_width) \
+			+ "\033[32m>\033[0m"  \
+			+ '\033[31m=\033[0m' * unfinished_width + ']')
 	sys.stdout.flush()
 	OUTPUT_LOCK.release()
+
+def open_dict(url):
+	global TOTAL
+	urls = []
+	with open('dict.txt', 'r') as f:
+			for eachline in f.readlines():
+				eachline = eachline.strip('\r\n')
+				if eachline[0:1]!='#':
+					urls.append(url+eachline)
+					TOTAL+=1
+
+	return urls
+	
+
+def send_request(url):
+	global COUNT
+	r = requests.get(url,headers = headers, allow_redirects = False,timeout = 60)
+	COUNT += 1
+	notfoundpagetext = requests.get(url + '/notexitstktdhhhhhh',allow_redirects = False).text
+	if r.status_code == 200 and r.text != notfoundpagetext:
+		print "%d %s\n"%(COUNT, url)
+		f.write(url)
+		f.write('\n')
+	if (COUNT % (TOTAL / WIDTH) == 0) :	
+		progress_bar()
 
 def main():
 	parser = argparse.ArgumentParser('the web scan start')
 	parser.add_argument("-u", "--url", type = str)
 	arg = parser.parse_args()
 	url = arg.url
-	r = send_request(url)
-	
-	urls = []
-	global TOTAL
-	with open('dict.txt', 'r') as infile:
-			line = infile.readline().strip('\r\n')
-			while(line):
-				if line[0:1]!='#':
-					urls.append(url+line)
-					TOTAL+=1
-				line = infile.readline().strip('\r\n')
-
+	urls = open_dict(url)
 	try:
-		pool = Pool(100)
-		pool.map(send_request, [url for url in urls])
-		progress_bar()
+		pool = Pool(1000) #创建拥有1000个进程数量的进程池
+		pool.map_async(send_request, [eurl for eurl in urls])
+		pool.close() #关闭进程池，不再接受新的进程
+		pool.join() #主进程阻塞，等待子进程的退出
 	except KeyboardInterrupt:
-		pool.terminate()
-	except ZeroDivisionError:
 		pool.terminate()
 	except Finished:
 		pool.terminate()
 
+	f.close()
 	print 'scan end'
 
 if __name__ == '__main__':
